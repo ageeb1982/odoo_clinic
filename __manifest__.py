4864335de815368705b472ef7d715cf9bf0115fc
Mohamed Khidir Ageeb <ageeb1982@gmail.com>
2020-08-08 12:29:29 +0300

    update 2020_08_08_12h_30m

--- __manifest__.py
+++ __manifest__.py
@@ -1,29 +1,22 @@
 # -*- coding: utf-8 -*-
 {
     'name': "العيادات",
-
+    'version': '1.22',
+    'category': 'Health',
+'author': "أبوالعباس محمد خضر عجيب",
+    'website': "https://ageebsoft.blogspot.com/",
     'summary': """
-       21 نبذه مختصرة عن المشروع الطبي""",
-
+       22 نبذه مختصرة عن المشروع الطبي""",
+    'sequence': 5,
     'description': """
        نبذه مطولة على المشروع الطبي   
        نبذه مطولة على المشروع الطبي
        نبذه مطولة على المشروع الطبي
-       نبذه مطولة على المشروع الطبي
-       نبذه مطولة على المشروع الطبي
-       نبذه مطولة على المشروع الطبي
-
-    
-    
     """,
 
 
-    'author': "أبوالعباس محمد خضر عجيب",
-    'website': "https://ageebsoft.blogspot.com/",
 
-    'version': '1.1',
-    'category': 'Health',
-    'version': '0.1',
+
     'images': [],
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -37,10 +30,21 @@
         'views/DoctorSpecialty_views.xml',
         'views/clinic_booking_view.xml',
 
+        # report
+        'report/clinic_booking_report.xml',
+        'report/clinic_booking_wiz_report.xml',
+        'wizard/clinic_booking_wiz_view.xml',
+
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    # 'css': ['static/src/css/.css'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'uninstall_hook': 'uninstall_hook',
+
 }
